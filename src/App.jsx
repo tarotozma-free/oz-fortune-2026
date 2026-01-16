00fc729995055d689a035b1cfcc81caeac3fa29d
Sena <tarotozma@gmail.com>
2026-01-16 14:33:30 +0900

    fix url

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://mwgvdtwxiiluwdxtbqgz.supabase.co',  // g로 수정!
-  'eyJhbGci...'
+  'https://mwgvdtwxiiluwdxtbqgz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13Z3ZkdHd4aWlsdXdkeHRicWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NDM2NzEsImV4cCI6MjA4NDAxOTY3MX0.XnK-V2r2Sb6Ndqw2HocTmrE2ujOLY-etBqpzD9dOZoo'
 );
 
 export default function App() {

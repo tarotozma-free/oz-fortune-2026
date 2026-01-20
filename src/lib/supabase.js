cb6b9fecd71c2f3f8ccc35459ccd24d6766bc3bc
Sena <tarotozma@gmail.com>
2026-01-20 14:17:49 +0900

    Add authentication and profile management1

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mwgvdtwxiiludxtbqgz.supabase.co'
+const supabaseUrl = 'https://mwgvdtwxiiluwdxtbqgz.supabase.co'
+//                               올바른 철자: iiluw
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13Z3ZkdHd4aWlsdXdkeHRicWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NDM2NzEsImV4cCI6MjA4NDAxOTY3MX0.XnK-V2r2Sb6Ndqw2HocTmrE2ujOLY-etBqpzD9dOZoo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
